9f0b57436b8d6e7f44d405d50aa2365001ffcf28
Jady Wright <iamjwright159@gmail.com>
2024-12-10 23:46:48 -0500

    Interactive terminal pog

--- texla-client/src/main.rs
+++ texla-client/src/main.rs
@@ -1,59 +1,71 @@
-use std::sync::mpsc;
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType};
+use std::io::{self, stdout, Write};
+use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
+use std::time::Duration;
+use tracing::{debug, warn};
+use tracing_subscriber::EnvFilter;
 use tungstenite::{connect, Message};
 
 fn main() {
+    crossterm::terminal::enable_raw_mode().unwrap();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let (mut socket, response) = connect("ws://localhost:8080/socket").expect("Can't connect");
     match socket.get_mut() {
         tungstenite::stream::MaybeTlsStream::Plain(stream) => {
             stream.set_nonblocking(true).unwrap();
         }
-        _ => println!("Can't set nonblocking mode for TLS stream"),
+        _ => warn!("Can't set nonblocking mode for TLS stream"),
     }
 
-    println!("Connected to the server");
-    println!("Response HTTP code: {}", response.status());
-    println!("Response contains the following headers:");
+    debug!("Connected to the server");
+    debug!("Response HTTP code: {}", response.status());
+    debug!("Response contains the following headers:");
     for (header, _value) in response.headers() {
-        println!("* {header}");
+        debug!("* {header}");
     }
 
     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || loop {
+    thread::spawn(move || 'thread: loop {
         loop {
             match rx.try_recv() {
                 Ok(msg @ Message::Text(_)) => {
-                    println!("Sent: {msg}");
                     socket.send(msg).expect("Can't send message");
                 }
-                Ok(Message::Close(_)) | Err(mpsc::TryRecvError::Disconnected) => {
+                Ok(Message::Close(_)) | Err(TryRecvError::Disconnected) => {
                     socket.close(None).expect("Can't close");
                 }
                 Ok(msg) => {
-                    println!("Sent unsupported message type: {msg}");
+                    warn!("Sent unsupported message type: {msg}");
                 }
-                Err(mpsc::TryRecvError::Empty) => break,
+                Err(TryRecvError::Empty) => break,
             }
         }
 
         loop {
             match socket.read() {
                 Ok(Message::Text(msg)) => {
-                    println!("Received: {msg}");
+                    execute!(io::stdout(), Clear(ClearType::CurrentLine)).unwrap();
+                    println!("\r{msg}");
                 }
                 Ok(msg) => {
-                    println!("Received unsupported message type: {msg}");
+                    warn!("Received unsupported message type: {msg}");
                 }
                 Err(tungstenite::Error::ConnectionClosed) => {
-                    println!("Connection closed");
-                    return;
+                    warn!("Connection closed");
+                    break 'thread;
                 }
-                Err(tungstenite::Error::Io(e)) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                Err(tungstenite::Error::Io(e)) if e.kind() == io::ErrorKind::WouldBlock => {
                     break;
                 }
                 Err(e) => {
-                    println!("Error: {e}");
+                    warn!("Error: {e}");
                     socket.close(None).expect("Can't close");
                 }
             }
@@ -61,9 +73,39 @@ fn main() {
     });
 
     loop {
+        use crossterm::event::{poll, read, Event, KeyCode};
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        println!("Sending: {input}");
-        tx.send(Message::Text(input)).expect("Can't send message");
+
+        loop {
+            execute!(io::stdout(), Clear(ClearType::CurrentLine)).unwrap();
+            print!("\r> {input}");
+            stdout().flush().unwrap();
+
+            if poll(Duration::from_millis(50)).unwrap() {
+                if let Event::Key(event) = read().unwrap() {
+                    match event.code {
+                        KeyCode::Char('\n') | KeyCode::Enter => {
+                            println!();
+                            break;
+                        }
+                        KeyCode::Char(c) => input.push(c),
+                        KeyCode::Backspace => {
+                            input.pop();
+                        }
+                        KeyCode::Esc => {
+                            crossterm::terminal::disable_raw_mode().unwrap();
+                            println!();
+                            return;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        input = input.trim().to_owned();
+        tx.send(Message::Text(input.clone()))
+            .expect("Can't send message");
+        input.clear();
     }
 }
--- texla-server/src/main.rs
+++ texla-server/src/main.rs
@@ -1,16 +1,18 @@
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 use bevy_ws_server::{ReceiveError, WsConnection, WsListener, WsPlugin};
 
 fn main() {
-    println!("Hello, world!");
     App::new()
-        .add_plugins((MinimalPlugins, WsPlugin))
+        .add_plugins((MinimalPlugins, LogPlugin::default(), WsPlugin))
         .add_systems(Startup, startup)
         .add_systems(Update, receive_message)
         .run();
 }
 
 fn startup(listener: Res<WsListener>) {
+    debug!("Hello, world!");
+
     listener.listen("127.0.0.1:8080");
 }
 
@@ -19,7 +21,9 @@ fn receive_message(mut commands: Commands, connections: Query<(Entity, &WsConnec
         loop {
             match conn.receive() {
                 Ok(message) => {
-                    println!("Received: {message}");
+                    debug!("> {message}");
+                    conn.send(message.clone());
+                    std::thread::sleep(std::time::Duration::from_secs(3));
                     conn.send(message);
                 }
                 Err(ReceiveError::Empty) => break,
